c1539b48536b56745809464e703f41e77b6527c0
Frank Willian <frank19valentim@gmail.com>
2023-06-07 15:18:12 -0300

    Subindo db

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ mongoose.connect('url/banco', {
     useNewUrlParser: true
 }, () => console.log('Connected to database'));
 
+const UserSchme = new mongoose.Schema({ username: String})
+const User = mongoose.model('User', UserSchema)
+const TodoSchema = new mongoose.Schema({ description: String, done: Boolean, user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+}})
+const Todo = mongoose.model('Todo', TodoSchema)
+
 app.get('/', (req,res) => {console.log('hello world')});
 
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
