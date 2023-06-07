656b090b15849a0fe85dc9bc43d70558655b207b
Frank Willian <frank19valentim@gmail.com>
2023-06-07 15:36:46 -0300

    Login user

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
 mongoose.connect('url/banco', {
     useUnifiedTopoly: true,
     useNewUrlParser: true
@@ -20,6 +22,20 @@ const TodoSchema = new mongoose.Schema({ description: String, done: Boolean, use
 const Todo = mongoose.model('Todo', TodoSchema)
 
 app.get('/', (req,res) => {console.log('hello world')});
+
+app.post('/session', async (req,res) => {
+    const {username } = req.body
+    let user = ''
+    try {
+        user = await User.findOne({username})
+        if (!user) {
+            user = await User.create({username})
+        }
+        return res.status(200).send(user)
+    } catch(err){
+        return res.status(400).send(err)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
 
