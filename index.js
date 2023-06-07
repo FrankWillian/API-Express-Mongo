3ec78942ef9e25da76e721e611ad3c35447d9f25
Frank Willian <frank19valentim@gmail.com>
2023-06-07 14:56:16 -0300

    Mongoose commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 const PORT = 3333
 
 const app = express()
 
+mongoose.connect('url/banco', {
+    useUnifiedTopoly: true,
+    useNewUrlParser: true
+}, () => console.log('Connected to database'));
+
+app.get('/', (req,res) => {console.log('hello world')});
+
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
+
 
